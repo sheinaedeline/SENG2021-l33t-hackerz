ec2b62203c315f22c683674f2de0c663f0eb01d2
Veeraj Sharma <VeerajSharma1@Veerajs-MBP.gateway>
2020-03-21 19:38:25 +1100

    initializing app

---
+++ crud/client/src/router.js
@@ -0,0 +1,16 @@
+import Router from 'vue-router';
+import Ping from './components/Ping.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    }
+  ],
+});
