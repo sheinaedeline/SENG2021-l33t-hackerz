b621938aef7d7b0e94abf22c24b54674d7b4b79a
allenwu12 <allenwu564@hotmail.com>
2020-04-01 22:17:52 +1100

    fixed personal transactions

--- SourceCode_and_Documentation/app.py
+++ SourceCode_and_Documentation/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, redirect, request
 import ast
 app = Flask(__name__)
 
-transactions = [
+p_transactions = [
     {
         'name': 'Rent',
         'paidby': 'John',
@@ -12,7 +12,7 @@ transactions = [
         'num': 3
     },
     {
-        'name': 'Electricity',
+        'name': 'Electricity Bill',
         'paidby': 'Bianca',
         'members': ['John', 'Bianca', 'Michael'],
         'price': 294.79,
@@ -28,10 +28,77 @@ transactions = [
         'num': 3
     },
     {
-        'name': 'Water',
+        'name': 'Water Bill',
         'paidby': 'Michael',
         'members': ['John', 'Bianca', 'Michael'],
         'price': 306.40,
+        'ratio': 0.25,
+        'num': 3
+    }
+]
+
+s_transactions = [
+    {
+        'name': 'Rent',
+        'paidby': 'Bianca',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 1040,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Electricity Bill',
+        'paidby': 'Bianca',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 294.79,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Groceries',
+        'paidby': 'You',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 103.80,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Water Bill',
+        'paidby': 'Michael',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 306.40,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Gas Bill',
+        'paidby': 'You',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 420.69,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Rent',
+        'paidby': 'Bianca',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 1040,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Electricity Bill',
+        'paidby': 'Bianca',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 294.79,
+        'ratio': 0.25,
+        'num': 3
+    },
+    {
+        'name': 'Groceries',
+        'paidby': 'You',
+        'members': ['John', 'Bianca', 'Michael'],
+        'price': 103.80,
         'ratio': 0.25,
         'num': 3
     }
@@ -54,8 +121,13 @@ def breakdown(trans):
     trans = ast.literal_eval(trans)
     return render_template("breakdown.html", trans=trans)
 
-@app.route("/personal_transactions", methods=['GET', 'POST'])
-def personal_transactions():
+@app.route("/personal_transactions/<rem_trans>", methods=['GET', 'POST'])
+def personal_transactions(rem_trans):
+    if rem_trans != 'ignore':
+        rem_trans = ast.literal_eval(rem_trans)
+        s_transactions.append(rem_trans)
+        p_transactions.remove(rem_trans)
+
     if request.method == 'POST':
         data = request.form
         new_trans = {
@@ -66,9 +138,8 @@ def personal_transactions():
             'ratio': 0.25,
             'num': 3
         }
-        transactions.append(new_trans)
-
-    return render_template("PersonalTransactions.html", transactions=transactions)
+        p_transactions.append(new_trans)            
+    return render_template("PersonalTransactions.html", transactions=p_transactions)
 
 @app.route("/review/<trans>")
 def review(trans):
@@ -77,7 +148,7 @@ def review(trans):
 
 @app.route("/shared_transactions")
 def shared_transactions():
-    return render_template("Shared_Transaction_Hist.html")
+    return render_template("Shared_Transaction_Hist.html", transactions=s_transactions)
 
 if __name__ == '__main__':
     app.run(debug=True)
