1d2ab940df4a67ef38b06e46c3e7ac305dedaef0
Veeraj Sharma <VeerajSharma1@Veerajs-MBP.modem>
2020-04-22 22:05:00 +1000

    dashboard frontend changes

--- newFrontend/src/components/mica/my-dashboard/MyDashboard.vue
+++ newFrontend/src/components/mica/my-dashboard/MyDashboard.vue
@@ -1,23 +1,42 @@
 <template>
-  <div class="dashboard">
+  <div>
     <div class="row">
-      <va-list class="display-5" fit>
-        <va-list-label>
-          Balances
-        </va-list-label>
-        <va-item v-for="(owing,name) in totals[$myName]" :key="name.concat('Owing')" clickable>
-          <va-item-section>
-            <va-item-label>
-              {{parseOwingString(owing,name)}}
-            </va-item-label>
-          </va-item-section>
-          <br><br>
-        </va-item>
-        <va-button outline :to="{ name: 'add-transactions' }">
-          Add a payment
-        </va-button>
-      </va-list>
+      <div class="dashboard flex md4">
+        <va-list class="display-5" fit>
+          <va-list-label>
+            Balances
+          </va-list-label>
+          <va-item v-for="(owing,name) in totals[$myName]" :key="name.concat('Owing')" clickable>
+            <va-item-section>
+              <va-item-label>
+                {{parseOwingString(owing,name)}}
+              </va-item-label>
+            </va-item-section>
+            <br><br>
+          </va-item>
+          <va-button outline :to="{ name: 'add-transactions' }">
+            Add a payment
+          </va-button>
+        </va-list>
+      </div>
+      <div class="va-card flex offset--md2 md6">
+        <!-- INFOBLOCKS-->
+        <div
+          class="flex offset--md4 md4"
+          v-for="(info, idx) in infoTiles"
+          :key="idx"
+        >
+          <va-card class="mb-0" :color="info.color">
+            <p class="display-2 mb-0" style="color: white;">{{ info.value }}</p>
+            <p>{{$t(info.text)}}</p>
+          </va-card>
+        </div>
+      </div>
+    </div>
+    <br/><br/>
       <!-- RULES -->
+  <div class="row">
+    <div class="dashboard flex md4">
       <va-list class="display-5" fit>
         <va-list-label>
           My house rules
@@ -40,7 +59,13 @@
         </template>
       </va-list>
     </div>
+    <!-- PIEGRAPH -->
+    <div class="va-card flex offset--md2 md6">
+
+    </div>
   </div>
+</div>
+</div>
 </template>
 
 <script>
@@ -49,6 +74,22 @@ export default {
   name: 'my-dashboard',
   data () {
     return {
+    infoTiles: [{
+      color: 'success',
+      value: '29',
+      text: 'Shared transactions',
+      icon: '',
+    }, {
+      color: 'danger',
+      value: '$204.36',
+      text: 'Personal contributions',
+      icon: '',
+    }, {
+      color: 'info',
+      value: '4',
+      text: 'Housemates',
+      icon: '',
+    }],
       totals: [],
       users: [],
       rules: [],
